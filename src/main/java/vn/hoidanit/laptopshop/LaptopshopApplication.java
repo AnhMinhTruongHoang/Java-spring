710072522828ecf145a2833bcf64ab2b4c6999e2
AnhMinhTruongHoang <truonghoanganhminh2000@gmail.com>
2024-10-20 01:14:29 +0700

    20/10

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 
-@SpringBootApplication(exclude =
-org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)	
+@SpringBootApplication(exclude = {
+	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+	org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+	})
+	
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -0,0 +1,17 @@
+package vn.hoidanit.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserService userService;
+
+
+
+        public String handleHello(){
+            return "hello service";
+        }
+
+       
+}
